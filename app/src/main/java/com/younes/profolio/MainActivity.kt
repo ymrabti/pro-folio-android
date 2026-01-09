99158685857e797f6712fbc7ba52dc86d399acb2
Younes M'RABTI <younesmrabti50@gmail.com>
2026-01-10 00:38:23 +0100

    feat: add splash screen support with custom theme and dependencies

--- app/src/main/java/com/younes/profolio/MainActivity.kt
+++ app/src/main/java/com/younes/profolio/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.younes.profolio.ui.screens.PortfolioScreen
 import com.younes.profolio.ui.theme.DarkBackground
 import com.younes.profolio.ui.theme.ProFolioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
